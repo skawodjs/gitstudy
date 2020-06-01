94ece6ceb72b6a3ec9a27cf62b5a13c4625577bb
skawodjs <skawodjs@naver.com>
2020-06-01 15:09:24 +0900

    hello()

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+public class Hello {
+	public static void main(String[] args) {
+		hello();
+	}
+		public static void hello() {
+			System.out.println("hello~");
+		}
+		
+	}
+
+
