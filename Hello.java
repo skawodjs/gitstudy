a321a88a5b28e1a79cb3144b77bfdbecd7bf2962
skawodjs <skawodjs@naver.com>
2020-06-01 15:25:40 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -3,9 +3,15 @@ package gitTest;
 public class Hello {
 	public static void main(String[] args) {
 		hello();
+		helloName("kacao");
+		helloName("Line");
 	}
 		public static void hello() {
 			System.out.println("hello~");
+		}
+		
+		public static void helloName(String name) {
+			System.out.println("Hello~ " + name);
 		}
 		
 	}
